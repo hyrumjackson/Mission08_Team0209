c31caade73cf741449484cc4357ea28ab13bb4e8
hyrumjackson <hyrumjjackson@gmail.com>
2024-02-28 09:57:46 -0700

    Controller edits

--- Mission08_Team0209/Controllers/HomeController.cs
+++ Mission08_Team0209/Controllers/HomeController.cs
@@ -6,10 +6,35 @@ namespace Mission08_Team0209.Controllers
 {
 	public class HomeController : Controller
 	{
+		private ITaskRespository _repo;
+
+		public HomeController(ITaskRespository temp)
+		{
+			_repo = temp;
+		}
+
+		[HttpGet]
 		public IActionResult Index()
 		{
 			return View();
 		}
 
+		//[HttpPost]
+		//public IActionResult Index()
+		//{
+		//	return View();
+		//}
+
+		[HttpGet]
+		public IActionResult Tasks()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Quadrant()
+		{
+			return View();
+		}
 	}
 }
